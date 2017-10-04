03e3916f1f89752b7117f1832b77f4b0df7a3484
zxh0 <zhangxiuhong2013@gmail.com>
2017-10-04 23:48:55 +0800

    operators

--- src/luago/state/api_arith.go
+++ src/luago/state/api_arith.go
@@ -4,6 +4,45 @@ import "math"
 import . "luago/api"
 import "luago/number"
 
+type operator struct {
+	metamethod  string
+	integerFunc func(int64, int64) int64
+	floatFunc   func(float64, float64) float64
+}
+
+var (
+	iadd = func(a, b int64) int64 { return a + b }
+	fadd = func(a, b float64) float64 { return a + b }
+	isub = func(a, b int64) int64 { return a - b }
+	fsub = func(a, b float64) float64 { return a - b }
+	imul = func(a, b int64) int64 { return a * b }
+	fmul = func(a, b float64) float64 { return a * b }
+	fdiv = func(a, b float64) float64 { return a / b }
+	iunm = func(a, _ int64) int64 { return -a }
+	funm = func(a, _ float64) float64 { return -a }
+	band = func(a, b int64) int64 { return a & b }
+	bor  = func(a, b int64) int64 { return a | b }
+	bxor = func(a, b int64) int64 { return a ^ b }
+	bnot = func(a, _ int64) int64 { return ^a }
+)
+
+var operators = []operator{
+	operator{"__add", iadd, fadd},
+	operator{"__sub", isub, fsub},
+	operator{"__mul", imul, fmul},
+	operator{"__mod", number.IMod, number.FMod},
+	operator{"__pow", nil, math.Pow},
+	operator{"__div", nil, fdiv},
+	operator{"__idiv", number.IFloorDiv, number.FFloorDiv},
+	operator{"__band", band, nil},
+	operator{"__bor", bor, nil},
+	operator{"__bxor", bxor, nil},
+	operator{"__shl", number.ShiftLeft, nil},
+	operator{"__shr", number.ShiftRight, nil},
+	operator{"__unm", iunm, funm},
+	operator{"__bnot", bnot, nil},
+}
+
 // [-(2|1), +1, e]
 // http://www.lua.org/manual/5.3/manual.html#lua_arith
 func (self *luaState) Arith(op ArithOp) {
